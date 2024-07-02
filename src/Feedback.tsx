15a12082a05df4a2c9a35f76b63e9fa7c38c196f
a1740942002 <a1740942002@me.com>
2024-07-02 23:13:16 +0800

    feat: Refactor promise handling logic in Feedback component.
    - Implement new logic for handling promises in actor behavior
    - Create a new actor dedicated to handling promises
    - Subscribe to the promise actor for output updates

--- src/Feedback.tsx
+++ src/Feedback.tsx
@@ -4,7 +4,7 @@
 // - Receive messages, change its "behavior" based on that message
 // - Spawn other actors
 
-import { createActor, fromTransition } from 'xstate'
+import { createActor, fromPromise, fromTransition } from 'xstate'
 
 // 1. Create actor logic ( like a reducer )
 const countLogic = fromTransition(
@@ -17,16 +17,32 @@ const countLogic = fromTransition(
   { count: 0 }
 )
 
+const promiseLogic = fromPromise(async () => {
+  const result = await new Promise((res) => {
+    setTimeout(() => {
+      res('done')
+    }, 1000)
+  })
+
+  return result
+})
+
 // 2. Create actor
 const countActor = createActor(countLogic)
+const promiseActor = createActor(promiseLogic)
 
 // 3. (Optional) Subscribe to a actor snapshot updates
 countActor.subscribe((snapshot) => {
   document.getElementById('output')!.innerHTML = String(snapshot.context.count)
 })
 
+promiseActor.subscribe((snapshot) => {
+  console.log(snapshot.output)
+})
+
 // 4. start the actor
 countActor.start()
+promiseActor.start()
 
 export function Feedback() {
   return (
