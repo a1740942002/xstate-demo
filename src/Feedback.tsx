922338151fef96bc694b7f77a5929576407db7b4
a1740942002 <a1740942002@me.com>
2024-07-02 16:23:34 +0800

    feat: Refactor feedback functionality and UI.
    - Implement counting functionality with logic and UI in Feedback component
    - Introduce increment button to increase count
    - Enhance user experience by displaying count in UI

--- src/Feedback.tsx
+++ src/Feedback.tsx
@@ -4,6 +4,43 @@
 // - Receive messages, change its "behavior" based on that message
 // - Spawn other actors
 
+import { createActor, fromTransition } from 'xstate'
+
+// 1. Create actor logic ( like a reducer )
+const countLogic = fromTransition(
+  (state, event) => {
+    if (event.type === 'increment') {
+      return { count: state.count + 1 }
+    }
+    return state
+  },
+  { count: 0 }
+)
+
+// 2. Create actor
+const countActor = createActor(countLogic)
+
+// 3. (Optional) Subscribe to a actor snapshot updates
+countActor.subscribe((snapshot) => {
+  document.getElementById('output')!.innerHTML = String(snapshot.context.count)
+})
+
+// 4. start the actor
+countActor.start()
+
 export function Feedback() {
-  return <div>Feedback</div>
+  return (
+    <div>
+      <div>
+        Output: <output id="output"></output>
+      </div>
+      <button
+        onClick={() => {
+          countActor.send({ type: 'increment' })
+        }}
+      >
+        +
+      </button>
+    </div>
+  )
 }
